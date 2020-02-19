a5770343b078ca407a375366587c3c22a6553a55
Joe Keenan <joe@flyingdiver.com>
2020-02-19 15:53:33 -0500

    removed unused merge routine

--- MQTT Connector.indigoPlugin/Contents/Server Plugin/plugin.py
+++ MQTT Connector.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -52,21 +52,6 @@ def makeVarForJSON(variable):
     var_data['value'] = variable.value
     return var_data
     
-def merge(a, b, path=None):
-    "merges b into a"
-    if path is None: path = []
-    for key in b:
-        if key in a:
-            if isinstance(a[key], dict) and isinstance(b[key], dict):
-                merge(a[key], b[key], path + [str(key)])
-            elif a[key] == b[key]:
-                pass # same leaf value
-            else:
-                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
-        else:
-            a[key] = b[key]
-    return a
-
 def deep_merge_dicts(original, incoming):
     """
     Deep merge two dictionaries. Modifies original.
